6a2f149b979fb1b0108a367a78ff3cfa01a41f9c
Steve McKenzie <steve@stevemckenzie.ca>
2020-06-15 20:01:34 -0700

    Updates

---
+++ precache-manifest.56d65110f1414616f7e4d2938d7e1311.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "528ff45e46f1c7341dd6d8a7b6c4a9c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13311aabb9f81b528b84",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "408d3b71a01941cf7c72",
+    "url": "/static/js/2.fe60f016.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fe60f016.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13311aabb9f81b528b84",
+    "url": "/static/js/main.2454ee54.chunk.js"
+  },
+  {
+    "revision": "324d21af555324e20b2b",
+    "url": "/static/js/runtime-main.393e0701.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
