0810fbb6323f2b4edf02c7c8b5617aa26d53d269
Steve McKenzie <steve@stevemckenzie.ca>
2020-06-21 15:42:58 -0700

    add filtering for only .jpg

--- findPhotos.js
+++ findPhotos.js
@@ -3,9 +3,12 @@
  * used, using node, fetch a list of photos and add them as an environment variable.
  */
 const fs = require('fs');
+const path = require('path');
 
 fs.readdir('./public/photos', (err, files) => {
-  const photos = files.join(' ');
+  const photos = files
+    .filter((file) => path.extname(file).toLowerCase() === '.jpg')
+    .join(' ');
 
   fs.writeFile('./.env.local', `REACT_APP_PHOTOS=${photos}`, (err, files) => {
     console.log('PHOTOS=', photos);
