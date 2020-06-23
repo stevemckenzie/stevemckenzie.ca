7c21116a043ea2b5f3bae7c25748bcf9bb2953b0
Steve McKenzie <steve@stevemckenzie.ca>
2020-06-23 12:23:12 -0700

    remove manually controlling `useKeyboardArrows`

--- src/Photos/index.js
+++ src/Photos/index.js
@@ -39,7 +39,6 @@ const Photos = () => {
       {...DEFAULT_CAROUSEL_SETTINGS}
       photos={photos}
       showIndicators={showIndicators}
-      useKeyboardArrows={showIndicators}
     />
   );
 };
